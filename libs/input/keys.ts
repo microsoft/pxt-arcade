88c0baf4d0bfac3eedeea345527f40b7ea3d6e20
Michal Moskal <michal@moskal.me>
2018-02-23 17:57:57 -0800

    Split keys into input package

--- libs/input/keys.ts
+++ libs/input/keys.ts
@@ -51,19 +51,19 @@ namespace keys {
     export const A = new Key(5)
     export const B = new Key(6)
 
-    export function dx(step = 1) {
+    export function dx(step = 100) {
         if (keys.Left.isPressed())
             if (keys.Right.isPressed()) return 0
-            else return -step
-        else if (keys.Right.isPressed()) return step
+            else return -step * control.deltaTime
+        else if (keys.Right.isPressed()) return step * control.deltaTime
         else return 0
     }
 
-    export function dy(step = 1) {
+    export function dy(step = 100) {
         if (keys.Up.isPressed())
             if (keys.Down.isPressed()) return 0
-            else return -step
-        else if (keys.Down.isPressed()) return step
+            else return -step * control.deltaTime
+        else if (keys.Down.isPressed()) return step * control.deltaTime
         else return 0
     }
 
