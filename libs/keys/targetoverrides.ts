e4b70f0917c971956c43352c5f4ac1e5149b79b1
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-03-15 15:48:44 -0700

    lowercase names for keys (#26)

--- libs/keys/targetoverrides.ts
+++ libs/keys/targetoverrides.ts
@@ -1,12 +1,12 @@
 namespace keys {
     //% fixedInstance block="left"
-    export const Left = new Key(1)
+    export const left = new Key(1)
     //% fixedInstance block="up"
-    export const Up = new Key(2)
+    export const up = new Key(2)
     //% fixedInstance block="right"
-    export const Right = new Key(3)
+    export const right = new Key(3)
     //% fixedInstance block="down"
-    export const Down = new Key(4)
+    export const down = new Key(4)
     //% fixedInstance block="A"
     export const A = new Key(5)
     //% fixedInstance block="B"
