a88bb8faa21db1b9981b8b49d8321711d7f11e9a
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-03-15 20:35:56 -0700

    Keys cpp support (#32)
    * moving keys/turtle to pxt-common-packages
    
    * updated common packages

--- libs/keys/enums.ts
+++
@@ -1,4 +0,0 @@
-const KEY_UP = 2048;
-const KEY_DOWN = 2049;
-const INTERNAL_KEY_UP = 2050;
-const INTERNAL_KEY_DOWN = 2051;
--- sim/simulator.ts
+++ sim/simulator.ts
@@ -1,5 +1,5 @@
 /// <reference path="../node_modules/pxt-core/built/pxtsim.d.ts"/>
-/// <reference path="../libs/keys/enums.ts"/>
+/// <reference path="../node_modules/pxt-common-packages/libs/keys/enums.ts"/>
 
 namespace pxsim {
     export type CommonBoard = Board
