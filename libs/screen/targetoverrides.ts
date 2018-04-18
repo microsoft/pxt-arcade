c3f5a36483d6be3f92d6bcd30425985be4469e4d
Richard Knoll <riknoll@users.noreply.github.com>
2018-04-18 16:56:59 -0700

    Fix image shim annotation (#89)

--- libs/screen/targetoverrides.ts
+++ libs/screen/targetoverrides.ts
@@ -1,7 +1,7 @@
 /**
  * Tagged image literal converter
  */
-//% shim=@f4 helper=image::ofBuffer blockIdentity="sprites._createImageShim"
+//% shim=@f4 helper=image::ofBuffer blockIdentity="images._spriteImage"
 //% groups=["0.","1#","2T","3t","4N","5n","6G","7g","8","9","aAR","bBP","cCp","dDO","eEY","fFW"]
 function img(lits: any, ...args: any[]): Image { return null }
 
