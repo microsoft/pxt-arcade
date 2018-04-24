8ea2c7f143198b6ea349c9f21f2678402f3e0edd
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-24 03:58:13 -0700

    upgraded pxt, remove pxtrequire

--- cmds/pxtrequire.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../node_modules/pxt-core/built/pxtlib.d.ts" />
-
-import * as path from "path";
-export let pxtCore = require("pxt-core");
-// require.resolve() gives path to [pxt dir]/built/pxt.js, so move up twice to get pxt root dir
-export let pxtCoreDir = path.resolve(require.resolve("pxt-core"), "..", "..");
