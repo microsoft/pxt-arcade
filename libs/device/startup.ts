6773224dbcca83293e8c7c4b397ad40c441a3382
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-01 14:00:53 -0700

    keys -> controller

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -1,4 +1,4 @@
 // This is the last thing executed before user code
 
-game.setWaitAnyKey(keys.pauseUntilAnyKey)
+game.setWaitAnyButton(controller.pauseUntilAnyButton)
 game.gameOverSound = () => music.playSound(music.sounds(Sounds.Wawawawaa));
