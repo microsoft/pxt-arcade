2a5d3982b57530e84ea4d4a064007785b30f2cd9
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-01 14:11:54 -0700

    fix build

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -1,4 +1,4 @@
 // This is the last thing executed before user code
 
-game.setWaitAnyButton(controller.pauseUntilAnyButton)
+game.setWaitAnyButton(controller.pauseUntilAnyButtonPressed)
 game.gameOverSound = () => music.playSound(music.sounds(Sounds.Wawawawaa));
