37d1239e2311dbc43d34613c66d037601ff9ece8
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-13 19:34:07 -0700

    fixing field editors

--- libs/core/ns.ts
+++ libs/core/ns.ts
@@ -6,21 +6,6 @@ namespace control {
 
 }
 
-namespace loops {
-    /**
-      * Get the time field editor
-      * @param ms time duration in milliseconds, eg: 500, 1000
-      */
-    //% blockId=timePicker block="%ms"
-    //% blockHidden=true
-    //% shim=TD_ID colorSecondary="#FFFFFF"
-    //% ms.fieldEditor="numberdropdown" ms.fieldOptions.decompileLiterals=true
-    //% ms.fieldOptions.data='[["100 ms", 100], ["200 ms", 200], ["500 ms", 500], ["1 second", 1000], ["2 seconds", 2000]]'
-    export function __timePicker(ms: number): number{
-        return ms;
-    }
-}
-
 //% color="#4b7bec"
 namespace music {
 
