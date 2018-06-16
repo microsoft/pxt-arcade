8d44b03a4c1e2f9691b187fd7bfb6daf1501054b
Michal Moskal <michal@moskal.me>
2018-06-16 10:49:58 -0700

    Exit/reset on player 1/2

--- libs/hw---rpi/keys.cpp
+++ libs/hw---rpi/keys.cpp
@@ -13,12 +13,14 @@ const int keyPins[] = {
     6,  // B=P31
     20, // SEL=P38
     26, // START=P37
+    23, // PLAYER1
+    22, // PLAYER2
     0};
 
 static uint32_t readBtns() {
     uint32_t r = 0;
     for (int i = 0; keyPins[i]; ++i) {
-        if (digitalRead(keyPins[i]))
+        if (!digitalRead(keyPins[i]))
             r |= 1 << (i + 1);
     }
     return r;
@@ -31,22 +33,31 @@ static void *btnPoll(void *dummy) {
     (void)dummy;
 
     uint32_t state = readBtns();
+    DMESG("btns: %p", state);
     while (1) {
         sleep_core_us(5000);
         uint32_t nstate = readBtns();
+
         if (state != nstate) {
             for (int i = 1; i < 32; ++i) {
                 uint32_t mask = 1 << i;
                 int ev = 0;
                 if ((state & mask) && !(nstate & mask))
                     ev = INTERNAL_KEY_UP;
-                else if (!(state & mask) && (nstate & mask))
+                else if (!(state & mask) && (nstate & mask)) {
                     ev = INTERNAL_KEY_DOWN;
+                    if (i == 7) // Player1
+                        target_exit();
+                    else if (i == 8) // Player2
+                        target_reset();
+                }
                 if (ev) {
+                    DMESG("evt: %d at %d", ev, i);
                     raiseEvent(ev, i);
                     raiseEvent(ev, 0); // any key
                 }
             }
+            state = nstate;
         }
     }
 
