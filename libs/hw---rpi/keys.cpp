9c04eecfcad4a3a71a9837abed99c1970f83259b
Michal Moskal <michal@moskal.me>
2018-06-16 00:39:39 -0700

    Keys impl

---
+++ libs/hw---rpi/keys.cpp
@@ -0,0 +1,69 @@
+#include "pxt.h"
+
+#include <wiringPi.h>
+#include <pthread.h>
+
+namespace pxt {
+
+const int keyPins[] = {
+    // Keys in order
+    13, // Y=P33
+    16, // X=P36
+    12, // A=P32
+    6,  // B=P31
+    20, // SEL=P38
+    26, // START=P37
+    0};
+
+static uint32_t readBtns() {
+    uint32_t r = 0;
+    for (int i = 0; keyPins[i]; ++i) {
+        if (digitalRead(keyPins[i]))
+            r |= 1 << (i + 1);
+    }
+    return r;
+}
+
+const int INTERNAL_KEY_UP = 2050;
+const int INTERNAL_KEY_DOWN = 2051;
+
+static void *btnPoll(void *dummy) {
+    (void)dummy;
+
+    uint32_t state = readBtns();
+    while (1) {
+        sleep_core_us(5000);
+        uint32_t nstate = readBtns();
+        if (state != nstate) {
+            for (int i = 1; i < 32; ++i) {
+                uint32_t mask = 1 << i;
+                int ev = 0;
+                if ((state & mask) && !(nstate & mask))
+                    ev = INTERNAL_KEY_UP;
+                else if (!(state & mask) && (nstate & mask))
+                    ev = INTERNAL_KEY_DOWN;
+                if (ev) {
+                    raiseEvent(ev, i);
+                    raiseEvent(ev, 0); // any key
+                }
+            }
+        }
+    }
+
+    return NULL;
+}
+
+void initKeys() {
+    wiringPiSetupGpio();
+
+    for (int i = 0; keyPins[i]; ++i) {
+        pinMode(keyPins[i], INPUT);
+        pullUpDnControl(keyPins[i], PUD_UP);
+    }
+
+    pthread_t disp;
+    pthread_create(&disp, NULL, btnPoll, NULL);
+    pthread_detach(disp);
+}
+
+} // namespace pxt
