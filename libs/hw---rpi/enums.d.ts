c72693e651cdffaf2ef0f3b9d43da48f15b819b3
Michal Moskal <michal@moskal.me>
2018-06-16 22:48:14 -0700

    Add APIs for game menu

---
+++ libs/hw---rpi/enums.d.ts
@@ -0,0 +1,28 @@
+// Auto-generated. Do not edit.
+
+
+    declare const enum Key {
+    Left = 1,
+    Up = 2,
+    Right = 3,
+    Down = 4,
+    A = 5,
+    B = 6,
+    Menu = 7,
+    Reset = 8,
+    Exit = 9,
+    }
+
+
+    declare const enum HwKey {
+    Y = 13,
+    X = 16,
+    A = 12,
+    B = 6,
+    Select = 20,
+    Start = 26,
+    Player1 = 23,
+    Player2 = 22,
+    }
+
+// Auto-generated. Do not edit. Really.
