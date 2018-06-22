b531296dd18189468abfea7bd5e507c0b7208877
Richard Knoll <riknoll@users.noreply.github.com>
2018-06-22 15:03:13 -0700

    Adding space and food images (#179)

---
+++ libs/device/bigFood.ts
@@ -0,0 +1,18 @@
+namespace sprites.food {
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigBurger = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigDrumstick = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigHam = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigPizza = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigTaco = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigCake = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigDonut = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=image.__imagePicker
+    export const bigIceCream = image.ofBuffer(hex``);
+}
