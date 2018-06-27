bc39c52beb46265acb274865a215be289029ad6d
Richard Knoll <riknoll@users.noreply.github.com>
2018-06-26 17:24:37 -0700

    Regenerating sprites with correct blockIdentity (#186)

--- libs/device/bigFood.ts
+++ libs/device/bigFood.ts
@@ -1,18 +1,18 @@
 namespace sprites.food {
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigBurger = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigDrumstick = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigHam = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigPizza = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigTaco = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigCake = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigDonut = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=image.__imagePicker
+    //% fixedInstance jres blockIdentity=images._image
     export const bigIceCream = image.ofBuffer(hex``);
 }
