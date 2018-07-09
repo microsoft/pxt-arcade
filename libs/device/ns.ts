f39ad43c57b8127425232d5975dc59c874c4fd3e
Richard Knoll <riknoll@users.noreply.github.com>
2018-07-09 15:17:34 -0700

    Removing categories (#197)

--- libs/device/ns.ts
+++ libs/device/ns.ts
@@ -1,11 +1,28 @@
 //% color=#8854d0
 namespace game {
+    /**
+     * Returns the time since the game started in milliseconds
+     */
+    //% blockId=arcade_game_runtime block="time since start (ms)"
+    //% group="Gameplay" weight=11
+    export function runtime(): number {
+        return control.millis();
+    }
 
+    /**
+     * Reset the current game. This is usually equivalent to pressing
+     * the reset button to restart the current program
+     */
+    //% blockId=arcade_game_reset block="reset game"
+    //% group="Gameplay" weight=10
+    export function reset() {
+        control.reset();
+    }
 }
 
 //% color="#4b6584"
 namespace scene {
-    
+
 }
 
 //% color="#cf6a87"
