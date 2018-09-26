c39f6f26d1d0a35ab909f819d51f3d97758cf2f4
Michal Moskal <michal@moskal.me>
2018-09-25 18:04:27 -0700

    Bump codal

--- libs/core---samd51/shims.d.ts
+++ libs/core---samd51/shims.d.ts
@@ -224,20 +224,6 @@ declare namespace control {
 declare namespace pins {
 
     /**
-     * Read `size` bytes from a 7-bit I2C `address`.
-     */
-    //% repeat.defl=0 shim=pins::i2cReadBuffer
-    function i2cReadBuffer(address: int32, size: int32, repeat?: boolean): Buffer;
-
-    /**
-     * Write bytes to a 7-bit I2C `address`.
-     */
-    //% repeat.defl=0 shim=pins::i2cWriteBuffer
-    function i2cWriteBuffer(address: int32, buf: Buffer, repeat?: boolean): int32;
-}
-declare namespace pins {
-
-    /**
      * Write to the SPI slave and return the response
      * @param value Data to be sent to the SPI slave
      */
