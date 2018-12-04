23aa5d20f2bde8d2a66af7999518d3e5ef3cda11
Michal Moskal <michal@moskal.me>
2018-12-04 12:07:35 -0800

    Align config with bootloader info; fixes #346

--- libs/hw---stm32f401/config.ts
+++ libs/hw---stm32f401/config.ts
@@ -2,29 +2,16 @@
 namespace config {
     export const PIN_LED = DAL.PC_8;
 
-    export const PIN_SCK = DAL.PA_5;
-    export const PIN_MISO = DAL.PA_6;
-    export const PIN_MOSI = DAL.PA_7;
+    export const PIN_JACK_TX = DAL.PA_9;
 
-    export const PIN_RX = DAL.PA_10;
-    export const PIN_TX = DAL.PA_9;
+    export const PIN_BTN_LEFT = DAL.PA_15;
+    export const PIN_BTN_UP = DAL.PA_5;
+    export const PIN_BTN_RIGHT = DAL.PC_13;
+    export const PIN_BTN_DOWN = DAL.PB_10;
 
-    export const PIN_SDA = DAL.PB_7;
-    export const PIN_SCL = DAL.PB_6;
-
-    export const PIN_JACK_TX = PIN_TX; // DAL.PA_9;
-
-    export const PIN_D14 = DAL.PB_9;
-    export const PIN_D15 = DAL.PB_8;
-
-    export const PIN_BTN_LEFT = DAL.PB_10;
-    export const PIN_BTN_UP = DAL.PA_15;
-    export const PIN_BTN_RIGHT = DAL.PA_5;
-    export const PIN_BTN_DOWN = DAL.PC_13;
-
-    export const PIN_BTN_A = PIN_SDA;
-    export const PIN_BTN_B = PIN_SCL;
-    export const PIN_BTN_MENU = PIN_RX;
+    export const PIN_BTN_A = DAL.PB_1;
+    export const PIN_BTN_B = DAL.PB_0;
+    export const PIN_BTN_MENU = DAL.PC_10;
 
     export const PIN_DISPLAY_CS = DAL.PB_12;
     export const PIN_DISPLAY_SCK = DAL.PB_13;
@@ -32,10 +19,8 @@ namespace config {
     export const PIN_DISPLAY_DC = DAL.PC_5;
     export const PIN_DISPLAY_RST = DAL.PC_4;
     export const PIN_DISPLAY_MISO = DAL.PB_14;
-    export const PIN_DISPLAY_BL = DAL.PA_4;
-    
-    // it's really piezo speaker, not an amp
-    export const PIN_SPEAKER_AMP = DAL.PB_8;
+    export const PIN_DISPLAY_BL = DAL.PC_7;
+   
 
     export const DISPLAY_CFG0 = 0x00000080;
     export const DISPLAY_CFG1 = 0x000603;
