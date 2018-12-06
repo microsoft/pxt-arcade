59071091c7272a4eeb905d212bde5199a0d8286f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-06 04:14:28 -0800

    inject volume items in menu

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -4,3 +4,14 @@ game.setWaitAnyButton(controller.pauseUntilAnyButtonIsPressed)
 game.gameOverSound = () => music.wawawawaa.play();
 // force first game engine
 game.eventContext()
+
+screen.systemMenu.addEntry(() => "volume up", function {
+    const v = music.volume();
+    music.setVolume(v + 32);
+    music.playTone(440, 500);
+});
+screen.systemMenu.addEntry(() => "volume down", () => {
+    const v = music.volume();
+    music.setVolume(v - 32);
+    music.playTone(440, 500);
+});
