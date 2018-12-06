f78c2709c9b221f800e2fa0a97e70212f234e372
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-06 04:18:11 -0800

    formatting

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -1,17 +1,21 @@
 // This is the last thing executed before user code
 
 game.setWaitAnyButton(controller.pauseUntilAnyButtonIsPressed)
-game.gameOverSound = () => music.wawawawaa.play();
+game.gameOverSound = function () { music.wawawawaa.play(); }
 // force first game engine
 game.eventContext()
 
-screen.systemMenu.addEntry(() => "volume up", function() {
-    const v = music.volume();
-    music.setVolume(v + 32);
-    music.playTone(440, 500);
-});
-screen.systemMenu.addEntry(() => "volume down", function () {
-    const v = music.volume();
-    music.setVolume(v - 32);
-    music.playTone(440, 500);
-});
+screen.systemMenu.addEntry(
+    function () { return "volume up" },
+    function () {
+        const v = music.volume();
+        music.setVolume(v + 32);
+        music.playTone(440, 500);
+    });
+screen.systemMenu.addEntry(
+    function () { return "volume down" },
+    function () {
+        const v = music.volume();
+        music.setVolume(v - 32);
+        music.playTone(440, 500);
+    });
