8a94f5d084b21fe66b693ac2b9b9d9946ab2a641
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-06 04:17:10 -0800

    typo

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -5,12 +5,12 @@ game.gameOverSound = () => music.wawawawaa.play();
 // force first game engine
 game.eventContext()
 
-screen.systemMenu.addEntry(() => "volume up", function {
+screen.systemMenu.addEntry(() => "volume up", function() {
     const v = music.volume();
     music.setVolume(v + 32);
     music.playTone(440, 500);
 });
-screen.systemMenu.addEntry(() => "volume down", () => {
+screen.systemMenu.addEntry(() => "volume down", function () {
     const v = music.volume();
     music.setVolume(v - 32);
     music.playTone(440, 500);
