cff3b9b0d17e3c8065bb18835cd1768ccd93aae9
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-20 14:20:11 -0800

    missing DAL constant

--- sim/dal.ts
+++ sim/dal.ts
@@ -7,4 +7,5 @@ namespace DAL {
     export const DEVICE_PIN_EVT_FALL = 3;
     export const DEVICE_PIN_EVT_PULSE_HI = 4;
     export const DEVICE_PIN_EVT_PULSE_LO = 5;
+    export const DEVICE_ID_IO_P0 = 100;
 }
