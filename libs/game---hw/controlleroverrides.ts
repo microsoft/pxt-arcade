4a168316e2a5ccadf077d6ca03ee7e2d5d3a4dd4
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-20 18:01:37 -0800

    restoring original buttons

--- libs/game---hw/controlleroverrides.ts
+++ libs/game---hw/controlleroverrides.ts
@@ -1,18 +1,18 @@
 namespace controller {
     //% fixedInstance whenUsed block="A"
-    export const A = new Button(ControllerButton.A);
+    export const A = new Button(ControllerButton.A, input.buttonA.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance whenUsed block="B"
-    export const B = new Button(ControllerButton.B);
+    export const B = new Button(ControllerButton.B, input.buttonB.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance whenUsed block="left"
-    export const left = new Button(ControllerButton.Left);
+    export const left = new Button(ControllerButton.Left, input.buttonLeft.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance whenUsed block="up"
-    export const up = new Button(ControllerButton.Up);
+    export const up = new Button(ControllerButton.Up, input.buttonUp.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance whenUsed block="right"
-    export const right = new Button(ControllerButton.Right);
+    export const right = new Button(ControllerButton.Right, input.buttonRight.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance whenUsed block="down"
-    export const down = new Button(ControllerButton.Down);
+    export const down = new Button(ControllerButton.Down, input.buttonDown.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
     //% fixedInstance whenUsed block="menu"
-    export const menu = new Button(6);
+    export const menu = new Button(7, input.buttonMenu.id(), DAL.DEVICE_BUTTON_EVT_UP, DAL.DEVICE_BUTTON_EVT_DOWN);
 
     //% fixedInstance whenUsed block="player 2"
     export const controller2 = new Controller(8);
