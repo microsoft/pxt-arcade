2e970a46430d624ab024a18bb70adad65a432213
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-20 17:59:34 -0800

    updated overrides

--- libs/game---hw/controlleroverrides.ts
+++ libs/game---hw/controlleroverrides.ts
@@ -1,8 +1,18 @@
 namespace controller {
-    function idToBtnId(id: number): number {
-        const btn = pxt.getButtonByPinCfg(id, DAL.BUTTON_ACTIVE_LOW_PULL_UP);
-        return btn.id();
-    }
+    //% fixedInstance whenUsed block="A"
+    export const A = new Button(ControllerButton.A);
+    //% fixedInstance whenUsed block="B"
+    export const B = new Button(ControllerButton.B);
+    //% fixedInstance whenUsed block="left"
+    export const left = new Button(ControllerButton.Left);
+    //% fixedInstance whenUsed block="up"
+    export const up = new Button(ControllerButton.Up);
+    //% fixedInstance whenUsed block="right"
+    export const right = new Button(ControllerButton.Right);
+    //% fixedInstance whenUsed block="down"
+    export const down = new Button(ControllerButton.Down);
+    //% fixedInstance whenUsed block="menu"
+    export const menu = new Button(6);
 
     //% fixedInstance whenUsed block="player 2"
     export const controller2 = new Controller(8);
@@ -11,5 +21,5 @@ namespace controller {
     //% fixedInstance whenUsed block="player 4"
     export const controller4 = new Controller(24);
     //% fixedInstance whenUsed block="player 1"
-    export const controller1 = new Controller(1, idToBtnId);
+    export const controller1 = new Controller(-1, [left, up, right, down, A, B, menu]);
 }
