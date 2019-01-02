989129c23589610eeb1adaadde4939f60fa508af
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-02 09:53:20 -0800

    mystery of tsconfig

--- sim/simulator.ts
+++ sim/simulator.ts
@@ -1,4 +1,4 @@
-/// <reference path="../node_modules/pxt-core/built/pxtsim.d.ts"/>
+// <reference path="../node_modules/pxt-core/built/pxtsim.d.ts"/>
 /// <reference path="../node_modules/pxt-common-packages/libs/game/constants.ts"/>
 
 namespace pxsim {
