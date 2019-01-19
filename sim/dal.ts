b776d5e2e387436cbec8f453c50712cf280f35f6
Abhijith Chatra <abchatra@microsoft.com>
2019-01-18 16:04:19 -0800

    Revert "Move sim to submodule (#668)" (#674)
    This reverts commit bb196f06ac71c4eadfca604746fdceae7c8d1557.

---
+++ sim/dal.ts
@@ -0,0 +1,11 @@
+namespace DAL {
+    export const DEVICE_PIN_EVENT_NONE = 0;
+    export const DEVICE_PIN_EVENT_ON_EDGE = 1;
+    export const DEVICE_PIN_EVENT_ON_PULSE = 2;
+    export const DEVICE_PIN_EVENT_ON_TOUCH = 3;
+    export const DEVICE_PIN_EVT_RISE = 2;
+    export const DEVICE_PIN_EVT_FALL = 3;
+    export const DEVICE_PIN_EVT_PULSE_HI = 4;
+    export const DEVICE_PIN_EVT_PULSE_LO = 5;
+    export const DEVICE_ID_IO_P0 = 100;
+}
