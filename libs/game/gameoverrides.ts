b7f30ffe27f1e4a557f5f239ec1c6c3890d957a0
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-25 21:01:55 -0700

    redefine forever

--- libs/game/gameoverrides.ts
+++ libs/game/gameoverrides.ts
@@ -8,25 +8,10 @@ function forever(a: () => void): void {
     game.forever(a);
 }
 
-/**
- * Pause for the specified time in milliseconds
- * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
- */
-//% help=loops/pause weight=99
-//% async block="pause %pause=timePicker|ms"
-//% blockId=device_pause blockNamespace="loops"
-function pause(ms: number): void {
-    loops.pause(ms);
-}
-
 // micro:bit compatibility
 // these functions allow some level of reuse
 // between micro:bit and other maker-style editors
 namespace basic {
-    export function pause(millis: number) {
-        loops.pause(millis);
-    }
-
     export function forever(a: () => void) {
         game.forever(a);
     }
