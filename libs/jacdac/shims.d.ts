07774cecb0cf9c2790d32074342cff79fff72f55
James Devine <j.devine@lancaster.ac.uk>
2019-05-06 23:34:56 -0700

    gen jacdac shims to include diagnostics func

--- libs/jacdac/shims.d.ts
+++ libs/jacdac/shims.d.ts
@@ -38,6 +38,12 @@ declare namespace jacdac {
     function __physStart(): void;
 
     /**
+     * Reads the diagnostics struct provided by the physical layer. Returns a buffer or NULL.
+     **/
+    //% shim=jacdac::__physGetDiagnostics
+    function __physGetDiagnostics(): Buffer;
+
+    /**
      * Stops the JACDAC physical layer.
      **/
     //% shim=jacdac::__physStop
