d26b463191a144268ac31a0a5ce4d060e219b806
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-08-01 14:23:43 -0700

    specialize feather support on arcade (#1171)
    * specialize feather support on arcade
    
    * adding weights
    
    * add required categories
    
    * define D2,D3

---
+++ libs/feather/targetoverrides.d.ts
@@ -0,0 +1,6 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D2)
+    const D2: PwmPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_D3)
+    const D3: PwmPin;
+}
