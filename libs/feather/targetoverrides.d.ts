3d536d342df2d19db9b6569e8a6ad82e60968c61
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-08-01 09:39:09 -0700

    specialize feather support on arcade

---
+++ libs/feather/targetoverrides.d.ts
@@ -0,0 +1,6 @@
+declare namespace pins {
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A8)
+    const A8: AnalogInPin;
+    //% fixedInstance shim=pxt::getPinCfg(CFG_PIN_A9)
+    const A9: AnalogInPin;
+}
