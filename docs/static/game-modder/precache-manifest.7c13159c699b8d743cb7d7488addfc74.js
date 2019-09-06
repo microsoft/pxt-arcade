bed42ee4265f1c3c56bbfc05bc7c7aada7969dcf
darzu <daryl.zuniga@gmail.com>
2019-09-06 15:36:22 -0700

    adds the bunny-hop game modder as static files to docs/static/

---
+++ docs/static/game-modder/precache-manifest.7c13159c699b8d743cb7d7488addfc74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ada2619b69e2e789c3eefd171a0d673",
+    "url": "/static/game-modder/index.html"
+  },
+  {
+    "revision": "cc46f61e82608fd6199c",
+    "url": "/static/game-modder/static/css/main.d7cfe8e4.chunk.css"
+  },
+  {
+    "revision": "4e7533b267d4b864efaa",
+    "url": "/static/game-modder/static/js/2.32b899f4.chunk.js"
+  },
+  {
+    "revision": "cc46f61e82608fd6199c",
+    "url": "/static/game-modder/static/js/main.efc60e06.chunk.js"
+  },
+  {
+    "revision": "21fc42b4348f8b2bfd43",
+    "url": "/static/game-modder/static/js/runtime~main.ed37a638.js"
+  }
+]);
