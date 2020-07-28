df9b9e02653d99a62757a7b65b5988d812a0bd00
jwunderl <joey.wunderlich@gmail.com>
2020-07-27 17:33:48 -0700

    stubbin out stuff

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -3,3 +3,7 @@
 game.setWaitAnyButton(controller.pauseUntilAnyButtonIsPressed)
 // force first game engine
 game.eventContext()
+
+game.eventContext().registerFrameHandler(UPDATE_SCREEN_PRIORITY + 1, () => {
+    multiplayer.postImage(screen)
+})
