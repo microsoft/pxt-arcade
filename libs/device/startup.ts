d51cb2c0061e56c0e2e4a3209f1404423e7c7960
Vivian Li <vivl@microsoft.com>
2020-07-28 11:35:11 -0700

    Add handler to update game screen

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -7,3 +7,17 @@ game.eventContext()
 game.eventContext().registerFrameHandler(scene.UPDATE_SCREEN_PRIORITY + 1, () => {
     multiplayer.postImage(screen)
 })
+// [1 byte]        [1 byte]     [2 bytes]       [2 bytes]       [2 bytes]
+// magic number    bb           width           height          buffer
+let imageHeader: number[] = [0x87, 0x04, 0x00, 0xA0, 0x00, 0x78, 0x00, 0x00];
+
+multiplayer.addImageHandler(multiplayerImageHandler);
+function multiplayerImageHandler(img: number[]) {
+    if (img.length != 160 * 120) {
+        return; // Not a valid image buffer.
+    }
+    let packetBytes = imageHeader.concat(img);
+    let packetBuffer = Buffer.fromArray(packetBytes);
+
+    scene.setBackgroundImage(image.ofBuffer(packetBuffer)); // combination of header buffer and data buffer
+}
