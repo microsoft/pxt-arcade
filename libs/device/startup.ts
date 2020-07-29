bdcad4c4a47d5d1ea71188e04ddcf64432e2bd2f
jwunderl <joey.wunderlich@gmail.com>
2020-07-29 11:14:49 -0700

    match client code

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -4,20 +4,9 @@ game.setWaitAnyButton(controller.pauseUntilAnyButtonIsPressed)
 // force first game engine
 game.eventContext()
 
-
-namespace testtesttest {
-    let lastSent = game.runtime();
-    let throttle = 100; // ms
-    game.eventContext().registerFrameHandler(scene.UPDATE_SCREEN_PRIORITY + 1, () => {
-        if (multiplayer.getOrigin() == "server") {
-            if (lastSent + throttle < game.runtime()) {
-                lastSent = game.runtime() + throttle;
-                multiplayer.postImage(screen, "broadcast-screen");
-            }
-        } else {
-            const im: Image = multiplayer.getCurrentImage();
-            scene.setBackgroundImage(im);
-        }
-    })
-
-}
+game.eventContext().registerFrameHandler(199, () => {
+    if (multiplayer.getOrigin() === "client") {
+        const im: Image = multiplayer.getCurrentImage();
+        scene.setBackgroundImage(im);
+    }
+})
