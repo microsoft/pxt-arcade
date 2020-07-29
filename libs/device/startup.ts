8adddaf71dc607621260d84a09f70460b91662b3
jwunderl <joey.wunderlich@gmail.com>
2020-07-28 22:31:21 -0700

    throttle to at most 10 post images per second for now

--- libs/device/startup.ts
+++ libs/device/startup.ts
@@ -4,11 +4,20 @@ game.setWaitAnyButton(controller.pauseUntilAnyButtonIsPressed)
 // force first game engine
 game.eventContext()
 
-game.eventContext().registerFrameHandler(scene.UPDATE_SCREEN_PRIORITY + 1, () => {
-    if (multiplayer.getOrigin() == "server") {
-        multiplayer.postImage(screen, "broadcast-screen");
-    } else {
-        const im: Image = multiplayer.getCurrentImage();
-        scene.setBackgroundImage(im);
-    }
-})
+
+namespace testtesttest {
+    let lastSent = game.runtime();
+    let throttle = 100; // ms
+    game.eventContext().registerFrameHandler(scene.UPDATE_SCREEN_PRIORITY + 1, () => {
+        if (multiplayer.getOrigin() == "server") {
+            if (lastSent + throttle < game.runtime()) {
+                lastSent = game.runtime() + throttle;
+                multiplayer.postImage(screen, "broadcast-screen");
+            }
+        } else {
+            const im: Image = multiplayer.getCurrentImage();
+            scene.setBackgroundImage(im);
+        }
+    })
+
+}
