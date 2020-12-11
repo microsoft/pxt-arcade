72d90694986fc6261f525d156a645cf23465a25b
Shannon Kao <shakao@microsoft.com>
2020-12-10 16:48:16 -0800

    Update skillmap built files with fix for iframe in non-US locales

--- docs/static/skillmap/pxtlib.js
+++ docs/static/skillmap/pxtlib.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -47,17 +58,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 var __spreadArrays = (this && this.__spreadArrays) || function () {
     for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
     for (var r = Array(s), k = 0, i = 0; i < il; i++)
@@ -101,8 +101,8 @@ var pxt;
                 if (!data)
                     pxt.aiTrackEvent(id);
                 else {
-                    var props_1 = defaultProps || {};
-                    var measures_1 = defaultMeasures || {};
+                    var props_1 = __assign({}, defaultProps) || {};
+                    var measures_1 = __assign({}, defaultMeasures) || {};
                     Object.keys(data).forEach(function (k) {
                         if (typeof data[k] == "string")
                             props_1[k] = data[k];
