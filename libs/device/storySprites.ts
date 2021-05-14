a3ba543198fb05a27a3a6767984bc0d23f329002
Richard Knoll <riknoll@users.noreply.github.com>
2021-05-14 11:43:45 -0700

    Adding new gallery assets (#3512)
    * Adding new gallery assets
    
    * more assets

---
+++ libs/device/storySprites.ts
@@ -0,0 +1,42 @@
+namespace storySprites {
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="misc"
+    export const mistletoe = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const halloween = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const holiday = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const holiday2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const birthday = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const world = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const field = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const valentines = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const lake = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const ocean = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const sky = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const castle = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="background"
+    export const castle2 = image.ofBuffer(hex``);
+
+}
