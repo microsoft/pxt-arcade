710ac3e5cd47776008091dc001f9a22bc2b19e1c
Ed Kaim <ed@sharplogic.com>
2022-05-24 13:00:46 -0700

    Apply design pass (#4769)
    * Apply design pass
    
    * Add tbody to tables and point to beta embed UX

--- kiosk/src/Components/MainMenu.tsx
+++ kiosk/src/Components/MainMenu.tsx
@@ -9,15 +9,13 @@ interface IProps {
 const MainMenu: React.FC<IProps> = ({ kiosk }) => {
     return(
         <div>
-            <div className="row">
-                <div className="column">
-                    <h1>Games</h1>
-                    <GameList kiosk={kiosk} />
-                </div>         
-                <div className="column">
-                    <h1>High Scores</h1>
-                    <HighScoresList kiosk={kiosk} /> 
-                </div>
+            <div>
+                <h1 className="mainMenuHighScoreHeader">SELECT A GAME</h1>
+                <GameList kiosk={kiosk} />
+            </div>
+            <div>
+                <h2 className="mainMenuHighScoreHeader">High Scores</h2>
+                <HighScoresList kiosk={kiosk} />
             </div>
         </div>
     )
