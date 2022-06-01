083f48d9c95ab543258901c8f8b10b7b7dc40cfa
Ed Kaim <ed@sharplogic.com>
2022-06-01 12:34:58 -0700

    Inlined fonts

--- kiosk/src/index.tsx
+++ kiosk/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './FontPressStart2P.css';
+import './FontShare.css';
 import './Kiosk.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
