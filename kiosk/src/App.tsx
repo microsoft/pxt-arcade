5038831b1f080a2bc88be9c0b3b90ed0ffa6a305
Richard Knoll <riknoll@users.noreply.github.com>
2022-08-18 13:50:51 -0700

    Add pub hash to kiosk to open any share game (#4916)
    * Add pub hash to kiosk to open any share game
    
    * Also handle game over

--- kiosk/src/App.tsx
+++ kiosk/src/App.tsx
@@ -13,6 +13,9 @@ function App() {
   const[state, setState] = useState(kioskSingleton.state);
 
   useEffect(() => {
+    window.onhashchange = onHashChange;
+    onHashChange();
+
     kioskSingleton.onNavigated = () => {
       setState(kioskSingleton.state);
     };
@@ -26,6 +29,14 @@ function App() {
     }
 
   return (<div />)
+}
+
+function onHashChange() {
+  const hash = window.location.hash;
+  const match = /pub:((?:\d{5}-\d{5}-\d{5}-\d{5})|(?:_[a-zA-Z0-9]+))/.exec(hash);
+  if (match) {
+    kioskSingleton.launchGame(match[1], true);
+  }
 }
 
 export default App;
