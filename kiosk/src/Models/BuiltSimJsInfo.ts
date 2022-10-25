0604d720d263798c596699f9fc0ff74b4559d6a5
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2022-10-25 14:12:36 -0700

    Kiosk now receiving and caching built game data.

---
+++ kiosk/src/Models/BuiltSimJsInfo.ts
@@ -0,0 +1,12 @@
+import { PxfObject } from "tls";
+
+export class BuiltSimJSInfo {
+    constructor(
+        public js: string,
+        public targetVersion?: string,
+        public funArgs?: string[],
+        public parts?: string[],
+        public usedBuiltinParts?: string[]
+    ) {
+    }
+}
