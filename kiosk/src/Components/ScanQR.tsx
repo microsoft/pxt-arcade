fccf151c52f789e4faa2cc091cda8d8c49c70765
Sarah Rietkerk <49178322+srietkerk@users.noreply.github.com>
2022-11-14 16:35:31 -0800

    Kiosk now using a new carousel (#5333)
    * Playing with the new scanner
    
    * Kiosk: splitting out scanning logic
    
    * Still playing with new qr scanner.
    
    * Kiosk: More playing with new packages.
    
    * Kiosk: New Carousel semi-working with game pad
    
    * Kiosk: new carousel working with keyboard and game pad navigation.
    
    * Kiosk: slides now freeze when button selected
    
    * Kiosk: removed extras from game list, scan qr just has current qr logic in it.
    
    * Kiosk: html5-qrcode now being used as the qrscanner, some other minor bug fixes
    
    * Third party notice updated
    
    * Kiosk: removed an unused import.
    
    * Kiosk: addressing pr comments
    
    * Kiosk: high scores not rendering on main page to avoid temptation

--- kiosk/src/Components/ScanQR.tsx
+++ kiosk/src/Components/ScanQR.tsx
@@ -1,10 +1,7 @@
 import { useState } from "react";
 import { Kiosk } from "../Models/Kiosk";
-import { KioskState } from "../Models/KioskState";
 import "../Kiosk.css";
-import { OnResultFunction, QrReader } from 'react-qr-reader';
-import { addGameToKioskAsync } from "../BackendRequests";
-
+import play from "./QrScanner";
 interface IProps {
     kiosk: Kiosk
 }
@@ -13,60 +10,29 @@ const ScanQR: React.FC<IProps> = ({ kiosk }) => {
     const fullUrlHash = window.location.hash;
     const urlHashList = /add-game:((?:[a-zA-Z0-9]{6}))/.exec(fullUrlHash);
     const kioskId = urlHashList?.[1];
-    const [qrResponse, setQrResponse] = useState("");
     const [scannerVisible, setScanner] = useState(false);
 
     const renderQrScanner = () => {
         setScanner(true);
-    }
-
-    const scannerResult: OnResultFunction = async (result, error) => {
-        if (!!result) {
-            const qrUrl = result.toString();
-            const shareId = /\/([^\/]+)\/?$/.exec(qrUrl)?.[1];
-            try {
-                addGameToKioskAsync(kioskId, shareId);
-                kiosk.navigate(KioskState.QrSuccess);
-            }
-            catch (error) {
-                setQrResponse("Unable to add game to kiosk. Please try again later");
-            }
-
-        }
-
-        if (!!error) {
-            setQrResponse("No QR Code Found");
-        }
+        play(kiosk, kioskId!);
     }
 
     return (
         <div className="scanQrPage">
-            <h2>Add your game to </h2>
-            <h1>Kiosk {kioskId}</h1>
-            <ol className="scanInstructions">
-                <li>Open your game on your computer</li>
-                <li>Go to share your game</li>
-                <li>Scan QR code from the share modal</li>
-            </ol>
-            {
-                !scannerVisible &&
-                <button className="scanQrButton" onClick={renderQrScanner} >Open camera to scan the qr code</button>
-            }
-
-            {
-                scannerVisible &&
-                <div className="qrScannerHolder">
-                    <QrReader constraints={{ facingMode: "environment" }}
-                                onResult={scannerResult}
-                                scanDelay={500}
-                                className="qrScanner"
-                                videoContainerStyle={{height: "100%", padding: "0"}}
-                    />
-                    <p>{qrResponse}</p>
-                </div>
-            }
-
-
+            <h2>Add game to </h2>
+            <h2>Kiosk {kioskId}</h2>
+            <div className="scanInstructions">
+                <ol>
+                    <li>Open your game on your computer</li>
+                    <li>Go to share your game</li>
+                    <li>Scan QR code from the share dailog</li>
+                </ol>
+                {
+                    !scannerVisible &&
+                    <button className="scanQrButton" onClick={renderQrScanner} >Open camera to scan the qr code</button>
+                }
+                <div id="qrReader"></div>
+            </div>
         </div>
     )
 }
