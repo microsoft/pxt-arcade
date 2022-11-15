cad0f85ff9ac0c4379d61d80ad9ae971ca45a0ee
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2022-11-14 17:01:44 -0800

    Kiosk: input element now on scan page for no phone case.

--- kiosk/src/Components/ScanQR.tsx
+++ kiosk/src/Components/ScanQR.tsx
@@ -2,6 +2,9 @@ import { useState } from "react";
 import { Kiosk } from "../Models/Kiosk";
 import "../Kiosk.css";
 import play from "./QrScanner";
+import { addGameToKioskAsync } from "../BackendRequests";
+import { KioskState } from "../Models/KioskState";
+
 interface IProps {
     kiosk: Kiosk
 }
@@ -11,10 +14,31 @@ const ScanQR: React.FC<IProps> = ({ kiosk }) => {
     const urlHashList = /add-game:((?:[a-zA-Z0-9]{6}))/.exec(fullUrlHash);
     const kioskId = urlHashList?.[1];
     const [scannerVisible, setScanner] = useState(false);
+    const [linkError, setLinkError] = useState(false);
+    const perisistentShareLen = 52;
+    const regularShareLen = 34;
 
     const renderQrScanner = () => {
         setScanner(true);
         play(kiosk, kioskId!);
+    }
+
+    const checkUrl = async () => {
+        const input = document.getElementById("kiosk-share-link") as HTMLInputElement;
+        const shareLink = input.value;
+        const shareId = /\/([^\/]+)\/?$/.exec(shareLink)?.[1];
+        if (shareId) {
+            setLinkError(false);
+            try {
+                await addGameToKioskAsync(kioskId, shareId);
+                kiosk.navigate(KioskState.QrSuccess);
+            } catch (error) {
+                console.log("Unable to add game to kiosk. Please try again later");
+            }
+        } else {
+            setLinkError(true);
+        }
+
     }
 
     return (
@@ -32,6 +56,26 @@ const ScanQR: React.FC<IProps> = ({ kiosk }) => {
                     <button className="scanQrButton" onClick={renderQrScanner} >Open camera to scan the qr code</button>
                 }
                 <div id="qrReader"></div>
+                <div className="linkOption">
+                    <p>No phone?</p>
+                    <label>Submit share link here</label>
+                    <input type="url"
+                        id="kiosk-share-link"
+                        placeholder="Ex: https://arcade.makecode.com/S36491-40385-33470-30269"
+                        pattern="https:///arcade.makecode.com/.*"
+                        maxLength={perisistentShareLen}
+                        minLength={regularShareLen}
+                        spellCheck={false}
+                        required
+                        title="Share Link"
+                        />
+                    <input type="submit" onClick={checkUrl}></input>
+                    {
+                        linkError &&
+                        <p>Incorrect format for a share link</p>
+                    }
+                </div>
+
             </div>
         </div>
     )
