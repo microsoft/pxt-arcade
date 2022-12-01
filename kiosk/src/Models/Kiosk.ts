9a65659b8b6c479412c1ab3648838e2e069db2e7
Sarah Rietkerk <49178322+srietkerk@users.noreply.github.com>
2022-12-01 14:56:56 -0800

    Kiosk: High Score interface improved (#5423)
    * Kiosk: entering high score changes started.
    
    * Kiosk: entering a new high score now more consistent
    
    * Kiosk: styling for entering a high score improved
    
    * Kiosk: high score instructions added in case people are unsure of what to do
    
    * Kiosk: first pass of review changes.
    
    * Kiosk: split out new score functionality into new component
    
    * Kiosk: new build for updated high score interface

--- kiosk/src/Models/Kiosk.ts
+++ kiosk/src/Models/Kiosk.ts
@@ -227,6 +227,21 @@ export class Kiosk {
         }
     }
 
+    exitToEnterHighScore(): void {
+        const launchedGameHighs = this.getHighScores(this.launchedGame);
+        const currentHighScore = this.mostRecentScores[0];
+        const lastScore = launchedGameHighs[launchedGameHighs.length - 1]?.score;
+        if (launchedGameHighs.length === configData.HighScoresToKeep 
+            && lastScore 
+            && currentHighScore < lastScore) {
+                this.exitGame(KioskState.GameOver);
+
+        } else {
+            this.exitGame(KioskState.EnterHighScore);
+        }
+
+    }
+
     gameOver(skipHighScore?: boolean): void {
         if (this.state !== KioskState.PlayingGame) { return; }
 
@@ -236,7 +251,7 @@ export class Kiosk {
         }
 
         if (!skipHighScore && this.mostRecentScores && this.mostRecentScores.length && (this.selectedGame!.highScoreMode !== "None")) {
-            this.exitGame(KioskState.EnterHighScore);
+            this.exitToEnterHighScore();
         }
         else {
             this.exitGame(KioskState.GameOver);
