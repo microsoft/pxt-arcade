a2f1bd0a9b141031eaec9ae231745f88fb8bf673
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2023-02-03 10:15:36 -0800

    Kiosk: backendrequests file renamed

---
+++ kiosk/src/BackendRequests.ts
@@ -0,0 +1,72 @@
+const kioskBackendEndpoint: string = "https://makecode.com/api/kiosk";
+const apiBackendEndpoint: string = "https://makecode.com/api"
+
+export const getGameCodeAsync = async (kioskCode: string) => {
+    const getGameCodeUrl = `${kioskBackendEndpoint}/code/${kioskCode}`; 
+    let response = await fetch(getGameCodeUrl);
+    if (!response.ok) {
+        const e =  new Error(response.statusText);
+        e.name = "PollError";
+        throw e;
+
+    } else {
+        const gameCode = (await response.json())?.code;
+        if (gameCode !== "0") {
+            return gameCode;
+        }
+        const e =  new Error("Invalid game code");
+        e.name = "PollError";
+        throw e;
+    }
+}
+
+export const generateKioskCodeAsync = async () => {
+    const codeGenerationUrl = `${kioskBackendEndpoint}/newcode`;
+    const response = await fetch(codeGenerationUrl);
+    if (!response.ok) {
+        const e =  new Error(response.statusText);
+        e.name = "KioskCodeGenError";
+        throw e;
+    } else {
+        try {
+            const newKioskCode = (await response.json()).code;
+            // console log here to see if this is the empty string
+            return newKioskCode;
+        }
+        catch (error) {
+            throw new Error("No code returned from the request.");
+        }
+    }
+}
+
+export const addGameToKioskAsync = async (kioskId: string | undefined, gameShareId: string | undefined) => {
+    const updateKioskUrl = `${kioskBackendEndpoint}/updatecode`;
+    try {
+        const response: Response = await fetch(updateKioskUrl, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "kioskCode": kioskId,
+                "shareId": gameShareId
+            }),
+        });
+        await response.json();
+    }
+    catch (error) {
+        //dispatch here
+        throw new Error("Failed to post game to the kiosk");
+    }
+}
+
+export const getGameDetailsAsync = async (gameId: string) => {
+    const gameDetailsUrl = `${apiBackendEndpoint}/${gameId}`;
+    const response  = await fetch(gameDetailsUrl);
+    if (!response.ok) {
+        throw new Error("Unable to fetch the game details");
+    } else {
+        return await response.json();
+    }
+}
