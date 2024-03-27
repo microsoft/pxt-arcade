1c4ac97354c2ecf98841e805a8279cf984663948
Tom Ball <tball@microsoft.com>
2024-03-27 08:42:25 -0700

    parameterized delay

--- libs/screen---st7735/targetoverrides.ts
+++ libs/screen---st7735/targetoverrides.ts
@@ -6,8 +6,9 @@
 function img(lits: any, ...args: any[]): Image { return null }
 
 // this wait needed in case of an ESD event for arcade shield
-// would be best to have this part of configuration
-control.waitMicros(300000)
+const delay = control.getConfigValue(DAL.CFG_DISPLAY_DELAY, 0)
+if (delay > 0)
+    control.waitMicros(delay * 1000)
 
 // set palette before creating screen, so the JS version has the right BPP
 image.setPalette(hex`__palette`)
