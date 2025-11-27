21dbd15c31f6fa9989905610b0bb3d09a7e187be
a012 <Aeagan0507@gmail.com>
2025-11-26 22:37:12 -0500

    Create geolocation.ts

---
+++ libs/geolocation.ts
@@ -0,0 +1,9 @@
+/// <reference path="../localtypings/pxtarget.d.ts"/>
+
+namespace pxsim.cpp {
+    const vmKeepFunctions: pxt.Map<number> = {
+        "location::gps",
+        "navigate::location"
+    }  
+}
+  
