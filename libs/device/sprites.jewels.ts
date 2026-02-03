510fb13ac2235db1953e7149dca362a6b5afaafd
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2026-02-03 23:13:12 +0000

    add the jewels to the miscellaneous category for tiles

---
+++ libs/device/sprites.jewels.ts
@@ -0,0 +1,20 @@
+namespace sprites.jewels {
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile misc"
+    export const jewel1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile misc"
+    export const jewel2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile misc"
+    export const jewel3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile misc"
+    export const jewel4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile misc"
+    export const jewel5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    //% tags="tile misc"
+    export const jewel6 = image.ofBuffer(hex``);
+}
