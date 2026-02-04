8a5f854574af88833c9296a8f8965faebd70d7b1
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2026-02-04 00:05:54 +0000

    update the jewel tiles to have the right blockidentity

--- libs/device/sprites.jewels.ts
+++ libs/device/sprites.jewels.ts
@@ -1,20 +1,20 @@
 namespace sprites.jewels {
-    //% fixedInstance jres blockIdentity=images._image
+    //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile misc"
     export const jewel1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._image
+    //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile misc"
     export const jewel2 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._image
+    //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile misc"
     export const jewel3 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._image
+    //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile misc"
     export const jewel4 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._image
+    //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile misc"
     export const jewel5 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._image
+    //% fixedInstance jres blockIdentity=images._tile
     //% tags="tile misc"
     export const jewel6 = image.ofBuffer(hex``);
 }
